f896738fd4782043fcc6f9d77ba0ca0f24fefd81
magamig <magamig@gmail.com>
2022-05-13 11:36:50 +0200

    add padding to EHU dataset to match scalings

--- main/dataset_EHU.py
+++ main/dataset_EHU.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import os
 import sys
 from pathlib import Path
@@ -37,21 +38,41 @@ if not os.path.exists(f"{GT_PATH}/Pavia.mat"):
 if not os.path.exists(f"{GT_PATH}/PaviaU.mat"):
     os.system(f"wget http://www.ehu.eus/ccwintco/uploads/e/ee/PaviaU.mat -P {GT_PATH}")
 
+def expand2fitscaling(img, background_color=0):
+    width, height = img.size
+    max_scaling = max(SCALINGS)
+    new_width = math.ceil(width/max_scaling) * max_scaling
+    new_height = math.ceil(height/max_scaling) * max_scaling
+    if new_height == height and new_width == width:
+        return img
+    else:
+        result = Image.new(img.mode, (new_width, new_height), background_color)
+        result.paste(img)
+        return result
+
  # downsampling HS image
 for mat_path in glob.iglob(f'{GT_PATH}/*.mat'):
     name = Path(mat_path).stem
     print(name)
     mat = scipy.io.loadmat(mat_path)
     hsi = mat[list(mat.keys())[-1]]
+    print(hsi.shape)
+    new_hsi = None
+    for i in range(hsi.shape[2]):
+        img = Image.fromarray(hsi[:,:,i]) # np to Image
+        img = expand2fitscaling(img)
+        img = np.expand_dims(np.asarray(img), axis=2) # Image to np
+        new_hsi = img if  new_hsi is None else np.concatenate((new_hsi, img), axis=2)
+    hsi = new_hsi
+    scipy.io.savemat(f'{GT_PATH}/{name}.mat', {list(mat.keys())[-1]: new_hsi})
+    print(hsi.shape)
     hsi = hsi.astype("float32") / hsi.max()
     for sf in SCALINGS:
         hsi_downsampled = None
         for i in range(hsi.shape[2]):
-            # from np to Image
-            img = Image.fromarray(hsi[:,:,i])
+            img = Image.fromarray(hsi[:,:,i]) # np to Image
             img = img.resize((hsi.shape[1]//sf, hsi.shape[0]//sf), Image.LANCZOS)
-            # from Image to np
-            img = np.expand_dims(np.asarray(img), axis=2)
+            img = np.expand_dims(np.asarray(img), axis=2) # Image to np
             hsi_downsampled = img if  hsi_downsampled is None else np.concatenate((hsi_downsampled, img), axis=2)
         scipy.io.savemat(f'{HS_PATH}/{sf}/{name}.mat', {"hsi": hsi_downsampled})
 
